904e084111e34859d53fde2a3d2718509c9c38f1
zhufengzhufeng <894918097@qq.com>
2016-11-06 13:12:50 +0800

    copy

---
+++ 9.http/1.server.js
@@ -0,0 +1,13 @@
+//需要创建一个http服务，node给我们提供了一个模块,http
+var http = require('http');
+var server = http.createServer(function (req,res) {
+    //req客户端的请求
+    //res是服务端的响应  res是个流 可写流  req是个流 可读流
+    /*res.writeHead(200,{'Content-Type':'text/plain;charset=utf8'});*/
+    //statusCode + setHeader = writeHead
+    res.setHeader('Content-Type','text/plain;charset=utf8');
+    res.statusCode = 200;
+    res.write('你好');
+    res.end('世界');
+});
+server.listen(8888,'localhost');
