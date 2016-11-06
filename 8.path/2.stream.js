c385ebc08c7c7175a0433e11153ce56cba906cb1
zhufengzhufeng <894918097@qq.com>
2016-11-06 10:57:57 +0800

    pause resume

--- 8.path/2.stream.js
+++ 8.path/2.stream.js
@@ -1,15 +1,24 @@
 //流是从上到下，始终是一个
 var fs = require('fs');
-var rs = fs.createReadStream('./name.txt',{highWaterMark:1,start:3,end:5}); //包前又包后
+var rs = fs.createReadStream('./name1.txt',{highWaterMark:1,start:3,end:5}); //包前又包后
 //rs不是读取到的数据,而是一个可读流的对象
 //相当在家里安了一个水管，默认是非流动模式
 //监听on data事件将水管打开，fs会以最快的速度不停的发射emit data事件
 //encoding:null 类型为buffer
 //highWaterMark 64*1024 默认读取64k
+var arr = [];
+//从非流动模式->流动模式
 rs.on('data',function (data) {
     //data是我们需要的数据
-    console.log(data); //buffer转换成字符串，调用toString方法
+    //console.log(data); //buffer转换成字符串，调用toString方法
+    arr.push(data);
 });
+//监听结束
 rs.on('end',function () {
    //获取峰这个汉字
+    console.log(Buffer.concat(arr).toString());
+});
+//监听错误
+rs.on('error',function (err) {
+    console.log(err);
 });
---
+++ 8.path/3.readStream.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var rs = fs.createReadStream('./name.txt',{highWaterMark:3});
+rs.setEncoding('utf8'); //设置编码格式
+//如果编码格式是utf8 最小水位线就是3
+var str = '';
+//暂停出水 开启出水
+rs.on('data',function (data) { //默认每次读取64k 是怎么读的，不停的以最快速度将内容读取到内存中
+    str+=data;
+    console.log('出水');
+    rs.pause(); //停止触发data事件
+});
+var timer = setInterval(function () {
+    rs.resume(); //恢复触发resume事件
+},1000);
+rs.on('end',function () {
+    console.log(str);
+    clearInterval(timer)
+});
+// pause resume data end error
