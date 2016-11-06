ee5ce405517552745a14ec2f6a77af3f4894660c
zhufengzhufeng <894918097@qq.com>
2016-11-06 18:19:35 +0800

    server test

--- 9.http/server.js
+++ 9.http/server.js
@@ -1,25 +1,16 @@
 var http = require('http');
 var fs = require('fs');
-var crypto = require('crypto');
 var path = require('path');
-/*var mime = {
-    '.css':'text/css',
-    '.js':'application/x-javascript'
-};*/
-//第三方插件 mime
 var mime = require('mime');
-var url = require('url');//专门用来处理路径
+var url = require('url');
 http.createServer(function (req,res) {
-    //不想要?后面的内容
     var pathname = url.parse(req.url,true).pathname;
     if(pathname == '/'){
         res.setHeader('Content-Type','text/html;charset=utf8');
         fs.createReadStream('./index.html').pipe(res);
     }else{
-        //req.url  /index.js  /favicon.ico
         fs.exists('.'+pathname,function (exists) {
             if(exists){
-                //查看当前路径的文件类型
                 res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
                 fs.createReadStream('.'+pathname).pipe(res);
             }else{
@@ -29,3 +20,4 @@ http.createServer(function (req,res) {
         })
     }
 }).listen(3001);
+// 第二期视频 第一周补课
---
+++ 9.http/url.js
@@ -0,0 +1,2 @@
+var url = require('url');
+console.log(url.parse('https://username:password@www.baidu.com:80/s?wd=asda&rsv_spt=1&rsv_iqid=0xccd747ea0003f7f2&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=0&rsv_sug3=5&rsv_sug1=3&rsv_sug7=100&rsv_t=995fOE3matZ4TphQT9S57NhSpD1s0cjKW%2BJFID1bxU1%2BJ1%2BwxG9ZctNkITPmwDXfOKYX&inputT=839&rsv_sug4=943#/hello/workd',true));
